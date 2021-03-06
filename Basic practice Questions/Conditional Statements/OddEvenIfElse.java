1d5c9f53f2f203bb7b7ed7607f79cff5108d2467
Poonam Nainwal <77105745+PoonamNainwal@users.noreply.github.com>
2021-03-06 22:23:14 +0530

    Create OddEvenIfElse.java

---
+++ Basic practice Questions/Conditional Statements/OddEvenIfElse.java
@@ -0,0 +1,20 @@
+ // ques:-1 find a number is odd or even
+     import java.util.*;
+     import java.lang.*;
+     class OddEvenIfElse{
+       public static void main{
+     int a;
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter a number:- ");
+     a=sc.nextInt();
+     if(a%2==0)
+     {
+         System.out.println("Number is even");
+     }
+     else
+     {
+         System.out.println("Number is odd");
+     }
+    }
+   }    
+     
