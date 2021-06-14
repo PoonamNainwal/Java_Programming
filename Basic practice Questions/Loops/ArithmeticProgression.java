b1bb648397ed8302fd016b4c7f3f5630045c9ed0
Poonam Nainwal <77105745+PoonamNainwal@users.noreply.github.com>
2021-06-14 15:57:46 +0530

    Create ArithmeticProgression.java
    Program to Print Arithmetic Progression (AP) Series
    eg:- 3,8,13,18,23,.....
           5,10,15,20,25,30,.......

---
+++ Basic practice Questions/Loops/ArithmeticProgression.java
@@ -0,0 +1,26 @@
+//Program to Print Arithmetic Progression (AP) Series
+    
+import java.util.*;
+
+public class ArithmeticProgression 
+{
+public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Program to print AP Series");
+        System.out.println("Enter a, d and n");
+        int a=sc.nextInt();
+        int d=sc.nextInt();
+        int n=sc.nextInt();
+        
+        int term=a;
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(term+",");
+            term=term+d;
+        }     
+        
+        
+    }
+  }
