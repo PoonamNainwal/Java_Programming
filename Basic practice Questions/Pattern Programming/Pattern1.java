7a5faea84e6f0f405278fa0dcf870df6e86f69af
Poonam Nainwal <77105745+PoonamNainwal@users.noreply.github.com>
2021-06-15 15:35:46 +0530

    Create Pattern1.java
    Pattern program 1

---
+++ Basic practice Questions/Pattern Programming/Pattern1.java
@@ -0,0 +1,19 @@
+/*WAP to print 1 2 3 4 5 
+               1 2 3 4 5
+               1 2 3 4 5
+               1 2 3 4 5
+               1 2 3 4 5     */
+
+public class Patterns1 {
+  
+    public static void main(String[] args) 
+    {
+      for(int i=1;i<=5;i++)
+      {
+          for(int j=1;j<=5;j++)
+          {
+            System.out.println(j+" ");
+          }
+          System.out.println("");
+      }
+    }
