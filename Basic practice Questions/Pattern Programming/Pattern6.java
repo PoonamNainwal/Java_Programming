ff64ad4f4093d1b66b95f2cf500f2107a85cbce2
Poonam Nainwal <77105745+PoonamNainwal@users.noreply.github.com>
2021-06-15 16:35:31 +0530

    Create Pattern6.java

---
+++ Basic practice Questions/Pattern Programming/Pattern6.java
@@ -0,0 +1,28 @@
+/*WAP to print pattern  1
+	                      2 3
+	                      4 5 6 
+	                      7  8  9 10
+	                     11 12 13 14 15	
+	 */
+		
+import java.util.*;
+public class Patterns {
+
+	public static void main(String[] args) {
+		int count=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j =1; j<=i;j++)
+			{
+				count++;
+				System.out.print(count+" ");
+			}
+			System.out.println("");
+		}
+	 
+	}
+
+}
